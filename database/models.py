836d6bdbe1330bf8b75c7a8d5600b412f5ab47ac
tyl90427 <tyler.clark324@topper.wku.edu>
2020-12-05 20:57:51 -0600

    Ready for show and tell

--- database/models.py
+++ database/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     published_date = models.CharField(max_length=10)
     places_to_purchase = models.CharField(max_length=50)
     lowest_price = models.DecimalField(max_digits = 8, decimal_places=2)
+    general_review = models.CharField(max_length=1500)
 
     class Meta:
         verbose_name_plural = "books"
